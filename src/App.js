d0e9d56fb6bd43a1a4bce925ca139ac512327115
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-18 18:28:47 -0800

    add NavBar Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <h1>React Routing Demonstration</h1>
+      <p>
+        <Link to="/">Home</Link>
+        &nbsp;|&nbsp;
+        <Link to="/about">About</Link>   
+      </p>
+      <BrowserRouter>
+      <Switch>
+      </Switch>
+    </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/NavbarComp.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+/**
+ * //////////////////////////////////////////////////////////
+ *      NAVIGATION BAR COMPONENT
+ * //////////////////////////////////////////////////////////
+ * @returns A Navigation Bar
+ */
+const NavBar = () => {
+    return (
+        <header>
+            <div className='navbar navbar-dark bg-dark box-shadow'>
+                <div className='container d-flex justify-content-between'>
+                    <div className='text-warning'>
+                        <h2>
+                            <strong>React Routing Practice</strong>
+                        </h2>
+                    </div>
+                    <div>
+                        <button className='btn btn-secondary round'><strong>Home</strong></button>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default NavBar
